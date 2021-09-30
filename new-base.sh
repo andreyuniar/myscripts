5211fdc545b1f5b673242938af605bbec99f4a15
andreyuniar <58914618+andreyuniar@users.noreply.github.com>
2021-09-30 19:38:58 +0700

    Update new-base.sh

--- new-base.sh
+++ new-base.sh
@@ -93,11 +93,11 @@ PTTG=1
 DEF_REG=0
 
 # Files/artifacts
-FILES=Image.gz
+FILES=Image.gz-dtb
 
 # Build dtbo.img (select this only if your source has support to building dtbo.img)
 # 1 is YES | 0 is NO(default)
-BUILD_DTBO=1
+BUILD_DTBO=0
 	if [ $BUILD_DTBO = 1 ]
 	then 
 		# Set this to your dtbo path. 
